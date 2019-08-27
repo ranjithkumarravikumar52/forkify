5bf438c8e2586d0084dd35083425305f160f9128
ranjithkumarravikumar52 <ranjithkumarravikumar52@gmail.com>
2019-08-27 19:04:51 +0530

    * added content in index.js to check conversion from ESNext to ES5. #4.

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 // Global app controller
 
 import x from './test'; //x value will hold 23 coming from test module
-console.log(`I imported ${x} from another module!`); //this command works only if we use some kind of bundler tools
+const y = 23;
+console.log(`I imported ${x} from another module! Variable y is ${y}`); //this command works only if we use some kind of bundler tools
