112ae6b4953d07fe347c9164f984ed00a0dd2bc2
ranjithkumarravikumar52 <ranjithkumarravikumar52@gmail.com>
2019-08-28 12:11:07 +0530

    #5. * deleted test.js * removed all contents in index.js

--- src/js/index.js
+++
@@ -1,5 +0,0 @@
-// Global app controller
-
-import x from './test'; //x value will hold 23 coming from test module
-const y = 23;
-console.log(`I imported ${x} from another module! Variable y is ${y}`); //this command works only if we use some kind of bundler tools
