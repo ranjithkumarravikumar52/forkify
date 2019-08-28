984ad560a8fddfb7df65418d58631eb757cb4d92
ranjithkumarravikumar52 <ranjithkumarravikumar52@gmail.com>
2019-08-28 17:39:54 +0530

    #6. * Cleared contents in js files

--- src/js/index.js
+++
@@ -1,8 +0,0 @@
-import string from './models/Search'; //we don't mention the file extension, omit .js
-console.log(`importing string : ${string}`);
-
-//importing multiple functions and variable
-// import {add as a, multiply as m, ID} from "./views/searchView";
-import * as searchView from "./views/searchView";
-console.log(`Using imported functions add : ${searchView.add(3, 4)}, multiply : ${searchView.multiply(3, 4)} and ID : ${searchView.ID}`);
-
--- src/js/views/searchView.js
+++
@@ -1,4 +0,0 @@
-export const add = (a, b) => a + b ; //exporting a named function
-export const multiply = (a, b) => a * b ; //exporting a named function
-export const subtract = (a, b) => a - b ; //exporting a named function
-export const ID = 23 ; //exporting a named variable
