2ca76e285fce2e525cba481209dae8e18b9a4c6d
ranjithkumarravikumar52 <ranjithkumarravikumar52@gmail.com>
2019-08-28 12:33:05 +0530

    #5. * aliasing imported functions in index.js

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,6 @@ import string from './models/Search'; //we don't mention the file extension, omi
 console.log(`importing string : ${string}`);
 
 //importing multiple functions and variable
-import {add, multiply, ID} from "./views/searchView";
-console.log(`Using imported functions add : ${add(3, 4)}, multiply : ${multiply(3, 4)} and ID : ${ID}`);
+import {add as a, multiply as m, ID} from "./views/searchView";
+console.log(`Using imported functions add : ${a(3, 4)}, multiply : ${m(3, 4)} and ID : ${ID}`);
 
