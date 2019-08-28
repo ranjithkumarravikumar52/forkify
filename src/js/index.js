a4cbae0e321d9c1ebe5fdb30d75e6e59b1219904
ranjithkumarravikumar52 <ranjithkumarravikumar52@gmail.com>
2019-08-28 12:34:38 +0530

    #5. * import * in index.js

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ import string from './models/Search'; //we don't mention the file extension, omi
 console.log(`importing string : ${string}`);
 
 //importing multiple functions and variable
-import {add as a, multiply as m, ID} from "./views/searchView";
-console.log(`Using imported functions add : ${a(3, 4)}, multiply : ${m(3, 4)} and ID : ${ID}`);
+// import {add as a, multiply as m, ID} from "./views/searchView";
+import * as searchView from "./views/searchView";
+console.log(`Using imported functions add : ${searchView.add(3, 4)}, multiply : ${searchView.multiply(3, 4)} and ID : ${searchView.ID}`);
 
