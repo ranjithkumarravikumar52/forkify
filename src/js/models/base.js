3957c96b1d12f3f18f89fa527314684973001f77
ranjithkumarravikumar52 <ranjithkumarravikumar52@gmail.com>
2019-08-30 16:42:49 +0530

    #11. * added DOM element for the parent tag of the spinner * added function for renderTheLoader(parent)

--- src/js/models/base.js
+++ src/js/models/base.js
@@ -2,5 +2,23 @@
 export const elements = { //named export
     searchInput: document.querySelector('.search__field'),
     searchForm:  document.querySelector('.search'),
-    searchResultList: document.querySelector('.results__list')
+    searchResultList: document.querySelector('.results__list'),
+    searchResults: document.querySelector('.results')
+};
+
+/**
+ * ajax loading spinner - to be reusable across different views
+ * @param parent - expecting parent argument here so that we can attach spinner to child of the parent
+ */
+export const renderTheLoader = parent => {
+    const loader = `
+        <div class = "loader">
+            <svg>
+                <use href="img/icons.svg#icon-cw"></use>
+            </svg>
+        </div>
+    `;
+
+    //attach the above loader to the parent
+    parent.insertAdjacentHTML('afterbegin', loader);
 };
